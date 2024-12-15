c88c882ce32ea6058b61f9b685021c7873fd7450
Nntienquang <nntienquang1704@gmail.com>
2024-12-15 16:04:55 +0700

    update bootstrap cho index

--- js/main.js
+++ js/main.js
@@ -1,118 +1,38 @@
-(function ($) {
-    "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
 
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
+  // Lắng nghe sự kiện cuộn trang
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+  
+    // Kiểm tra nếu cuộn trang xuống quá 50px
+    if (window.scrollY > 50) {
+      navbar.classList.add("sticky-top"); // Thêm lớp sticky-top khi cuộn xuống
+    } else {
+      navbar.classList.remove("sticky-top"); // Xóa lớp khi cuộn lên trên
+    }
+  });
+
+
+  // Hàm kiểm tra nếu phần tử đã vào vùng nhìn thấy của người dùng
+function isElementInView(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
+  }
+  
+  // Lấy tất cả các card
+  const cards = document.querySelectorAll('.card');
+  
+  // Kiểm tra khi cuộn trang
+  function checkCardsInView() {
+    cards.forEach(card => {
+      if (isElementInView(card)) {
+        card.classList.add('show'); // Thêm lớp 'show' khi card vào vùng nhìn thấy
+      }
     });
-    
-    
-    // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-    
-    $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
-
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        margin: 24,
-        dots: true,
-        loop: true,
-        nav : false,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-    
-})(jQuery);
+  }
+  
+  // Lắng nghe sự kiện cuộn trang
+  window.addEventListener('scroll', checkCardsInView);
+  
+  // Kiểm tra ngay khi tải trang để các card được hiển thị ngay nếu chúng ở trong vùng nhìn thấy khi tải
+  window.addEventListener('load', checkCardsInView);
