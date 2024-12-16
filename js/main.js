510e50a018c319da057da58c223cf646ea6a737e
Nntienquang <nntienquang1704@gmail.com>
2024-12-16 21:46:05 +0700

    update table time program.html

--- js/main.js
+++ js/main.js
@@ -34,3 +34,23 @@ document.addEventListener("DOMContentLoaded", () => {
     updateCounter();
   });
 });
+
+// Timetable Filter
+$('.timetable-controls ul li').on('click', function() {
+    var tsfilter = $(this).data('tsfilter');
+    $('.timetable-controls ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    if(tsfilter == 'all') {
+        $('.classtime-table').removeClass('filtering');
+        $('.ts-item').removeClass('show');
+    } else {
+        $('.classtime-table').addClass('filtering');
+    }
+    $('.ts-item').each(function(){
+        $(this).removeClass('show');
+        if($(this).data('tsmeta') == tsfilter) {
+            $(this).addClass('show');
+        }
+    });
+});
