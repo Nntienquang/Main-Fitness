bfdcc72a47c904f1755bcc1cbb05b795e87be8bb
thehieu05 <phamthehieu2005@gmail.com>
2024-12-25 20:33:42 +0700

    up form dk,dn

--- js/main.js
+++ js/main.js
@@ -77,3 +77,26 @@ function loadHTML() {
 }
 
 document.addEventListener("DOMContentLoaded", loadHTML);
+
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopp = document.querySelector('.btn');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+btnPopp.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', ()=> {
+  wrapper.classList.remove('active-popup');
+});
+
+
